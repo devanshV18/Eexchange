ea5303cec85971e396548fb5d853866f3810d2db
devanshV18 <devanshverma024@gmail.com>
2024-07-12 04:20:01 +0530

    landing page done partially

--- digitalexchange/src/app/page.tsx
+++ digitalexchange/src/app/page.tsx
@@ -1,8 +1,33 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { Link } from "lucide-react";
+import Link from "next/link";
+import { buttonVariants, Button } from "@/components/ui/button";
+import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
+
+
+
+const perks = [
+  {
+    name: 'Instant Delivery',
+    icon: ArrowDownToLine,
+    description: "Assets delivered in seconds to your email ready to download right away"
+  },
+
+  {
+    name: 'Guaranteed Quality',
+    icon: CheckCircle,
+    description: 'Assured quality assets and kits ensured by our team'
+  },
+
+  {
+    name: 'For the planet',
+    icon: Leaf,
+    description: "We've pledged to plant 1 tree on every 5 successful orders!"
+  }
+]
 
 export default function Home() {
   return (
+  <>
     <MaxWidthWrapper>
       <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
@@ -15,9 +40,33 @@ export default function Home() {
         </p>
 
         <div className="flex flex-col sm:flex-row gap-4 mt-6">
-          {/* <Link href=""></Link> */}
+          <Link href="/products" className={buttonVariants()}>Browse Trending</Link>
+          <Button variant='outline'>Our Quality Promise</Button>
         </div>
       </div>
+
+      {/* {TODO : List Products} */}
     </MaxWidthWrapper>
+
+    <section className="border-t border-gray-200 bg-gray-50">
+      <MaxWidthWrapper className="py-20">
+         <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
+              {perks.map((perk) => (
+                <div key={perk.name} className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
+                    <div className="md:flex-shrink-0 flex justify-center">
+                      <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900 cursor-pointer">
+                        {<perk.icon className="w-1/3 h-1/3"/>}
+                      </div>
+                    </div>
+                    <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+                      <h3 className="text-base font-medium text-blue-600">{perk.name}</h3>
+                      <p className="mt-3 text-sm text-muted-foreground text-black font-semibold">{perk.description}</p>
+                    </div>
+                </div>
+              ))}
+         </div>
+      </MaxWidthWrapper>
+    </section>
+  </>
   )
 }
