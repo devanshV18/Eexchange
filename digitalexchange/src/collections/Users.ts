1fc6a076bf89fc2fd1c960c606629c05844a4279
devanshV18 <devanshverma024@gmail.com>
2024-07-30 15:50:55 +0530

    verify email page ui done, now stsrting verifyEmail component to actually verify email

--- digitalexchange/src/collections/Users.ts
+++ digitalexchange/src/collections/Users.ts
@@ -5,7 +5,7 @@ export const Users: CollectionConfig = {
     auth: {
         verify: {
             generateEmailHTML: ({token}) => {
-                return `<p>Please Verify your Email</p>`
+                return `<a href='${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}'>Verify Account</a>`
             }
         }
     },
