9210b17b8c637a9e2df1ea63c67a6e8d540ba658
devanshV18 <devanshverma024@gmail.com>
2024-08-01 17:25:35 +0530

    created ui for verify email page cases

--- digitalexchange/src/components/VerifyEmail.tsx
+++ digitalexchange/src/components/VerifyEmail.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { trpc } from "@/trpc/client"
-import { XCircle } from "lucide-react"
+import { Loader2, XCircle } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 import { buttonVariants } from "./ui/button"
@@ -16,33 +16,52 @@ const VerifyEmail = ({token}: VerifyEmailProps) => {
     })
 
     if(false){
-        return (<div className="flex flex-col items-center gap-2">
-            <XCircle className='h-8 w-8 text-red-600'/>
-            <h3 className="font-semibold text-xl">There was a Problem</h3>
-            <p className="text-muted-foreground text-sm">
-                This token is not valid or might be expired.
-                Please try Again
-            </p>
-        </div>)
+        return (
+            <div className="flex flex-col items-center gap-2">
+                <XCircle className='h-8 w-8 text-red-600'/>
+                <h3 className="font-semibold text-xl">There was a Problem</h3>
+                <p className="text-muted-foreground text-sm">
+                    This token is not valid or might be expired.
+                    Please try Again
+                </p>
+            </div>
+        )
+    }
+
+    if(false){
+        return(
+            <div className="flex h-full flex-col items-center justify-center">
+                <div className="relative mb-4 h-60 w-60 text-muted-foreground">
+                    <Image
+                     src='/hippo-email-sent.png'
+                     fill
+                     alt="The email was sent"
+                    />
+                </div>
+
+                <h3 className="font-semibold text-2xl">You&apos;re all set!</h3>
+
+                <p className="text-muted-foreground text-center mt-1">Thank you for verifying your Email</p>
+                <Link className={buttonVariants({ className: 'mt-4'})} 
+                href='/sign-in'
+                >
+                    Sign In
+                </Link>
+            </div>
+        )
     }
 
     if(true){
-        <div className="flex h-full flex-col items-center justify-center">
-            <div className="relative mb-4 h-60 w-60 text-muted-foreground">
-                <Image
-                 src="/hippo-email-sent.png"
-                 fill
-                 alt="the email was sent"
-                />
+        return(
+            <div className="flex flex-col items-center gap-2">
+                <Loader2 className='animate-spin h-8 w-8 text-blue-600'/>
+                <h3 className="font-semibold text-xl">Verifying</h3>
+                <p className="text-muted-foreground text-xl text-black">
+                    This wont&apos;t take too long!
+                </p>
             </div>
-
-            <h3 className="font-semibold text-2xl">You&aps;re all set!</h3>
-            <p className="text-muted-foreground text-center mt-1">Thank you for verifying your email</p>
-
-            <Link className={buttonVariants({className: 'mt-4'})} href="/sign-in">Sign In</Link>
-        </div>
+        )
     }
-
 
 }
 
